45d180333e6dad2f7631ee227678294c844de59c
Ulaş Demir <84096934+JefHardy@users.noreply.github.com>
2023-06-15 10:41:38 +0300

    main.py

--- Python_Temelleri/Harf_Sayisi_Bul/main.py
+++ Python_Temelleri/Harf_Sayisi_Bul/main.py
@@ -1,1 +1,10 @@
+cumle = "sadasdkqwlkşsadkkşkcxşlkzkkkdasşdkaksmxözmmcxmmxömxq"
+kelime = dict()
 
+for i in cumle:
+    if i in kelime:
+        kelime[i] +=1
+    else:
+        kelime[i] = 1
+for i,j in kelime.items():
+    print(i,":",j)
