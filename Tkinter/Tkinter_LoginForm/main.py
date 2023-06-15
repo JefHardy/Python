7013a5e7c273edcd5265f586c5e20819206a77de
Ulaş Demir <84096934+JefHardy@users.noreply.github.com>
2023-06-15 11:03:30 +0300

    main.py

--- Tkinter/Tkinter_LoginForm/main.py
+++ Tkinter/Tkinter_LoginForm/main.py
@@ -1,1 +1,31 @@
+from tkinter import messagebox
+import tkinter
 
+def giris_yap():
+    if klogin.get() == "ulas" and slogin.get() == "123":
+        messagebox.showinfo(title=f"Hoşgeldiniz {klogin.get()}",message=f"Hoşgeldiniz {klogin.get()}")
+
+window = tkinter.Tk()
+window.minsize(width=300,height=300)
+window.eval('tk::PlaceWindow . center')
+FONT = ("Times new roman",20,"italic")
+baslik = tkinter.Label(text="LOGİN FORM",font=FONT)
+baslik.pack()
+
+
+klogin = tkinter.Entry(width=20)
+slogin = tkinter.Entry(width=20)
+
+
+kloginlabel = tkinter.Label(text="Kullanıcı adı  ",font=("Times new roman",10,"normal"))
+sloginlabel = tkinter.Label(text="Şifre",font=("Times new roman",10,"normal"))
+login = tkinter.Button(text="Giriş Yap",command=giris_yap)
+
+
+kloginlabel.pack()
+klogin.pack()
+sloginlabel.pack()
+slogin.pack()
+login.pack()
+
+window.mainloop()
