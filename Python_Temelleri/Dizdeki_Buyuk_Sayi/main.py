f20558501348384c0a4b45a844348e8a1a667809
Ulaş Demir <84096934+JefHardy@users.noreply.github.com>
2023-06-15 10:38:43 +0300

    main.py

--- Python_Temelleri/Dizdeki_Buyuk_Sayi/main.py
+++ Python_Temelleri/Dizdeki_Buyuk_Sayi/main.py
@@ -1,1 +1,11 @@
-
+sayilar =  [100,20,30,122,40,80,10,25,5,10,4,50,70,5]
+buyuk_sayi = 0
+for i in sayilar:
+   for y in sayilar:
+        if i > y:
+            y = i
+            buyuk_sayi= y
+        else:
+            i = y
+            buyuk_sayi = i
+print(buyuk_sayi)
